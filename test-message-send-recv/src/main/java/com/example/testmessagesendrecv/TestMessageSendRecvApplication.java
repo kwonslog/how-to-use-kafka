c07b874430a85bd823751e29ae05ef9c6070a15f
unknown <korpgm@gmail.com>
2023-06-30 23:25:50 +0900

    first commit

---
+++ test-message-send-recv/src/main/java/com/example/testmessagesendrecv/TestMessageSendRecvApplication.java
@@ -0,0 +1,13 @@
+package com.example.testmessagesendrecv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMessageSendRecvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMessageSendRecvApplication.class, args);
+	}
+
+}
---
+++ test-message-send-recv/src/test/java/com/example/testmessagesendrecv/TestMessageSendRecvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testmessagesendrecv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMessageSendRecvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
